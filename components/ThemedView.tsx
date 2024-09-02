3ee09149f51003e92983b190e91be43cb421cb5c
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-09-03 00:25:19 +0400

    test

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -14,8 +14,7 @@ export function ThemedView({
   ...otherProps
 }: ThemedViewProps) {
   const backgroundColor = useThemeColor(
-    { light:                   lightColor,
-                                         dark: darkColor },
+    { light:               lightColor, dark: darkColor },
     "background",
   );
 
