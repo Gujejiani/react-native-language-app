af51bb6a23e0b1d1894f02538695e0d701e0b54f
kakha <kakha.gujejiani.1@iliauni.edu.ge>
2024-09-24 23:40:06 +0400

    test

--- app/(tabs)/theme.tsx
+++ app/(tabs)/theme.tsx
@@ -6,7 +6,7 @@ export default function ThemeScreen() {
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
- <Image
+        <Image
           source={require("@/assets/images/partial-react-logo.png")}
           style={styles.reactLogo}
         />
